bf40ddee184481245dc52c7ecf2eecc64a55c0fa
Jedrzej Zawojski <jedrzej.zawojski95@gmail.com>
2021-03-10 19:17:54 +0100

    Add some tests for sql-injection

--- backend/driverRoutes/test/app.test.js
+++ backend/driverRoutes/test/app.test.js
@@ -64,16 +64,31 @@ const testDate = "2021/03/06";
 describe('Getting daily report', () => {
     before(() => {
         dbConn.removeRecords();
-        let params = {
-            originAddress: "San Francisco, CA",
-            destinationAddress: "San Diego, CA",
-            price: 100.00,
-            date: testDate,   
-        }
+        let params = [
+            {
+                originAddress: "San Francisco, CA",
+                destinationAddress: "San Diego, CA",
+                price: 100.00,
+                date: testDate  
+            },
+            {
+                originAddress: "San Francisco, CA",
+                destinationAddress: "San Diego, CA",
+                price: 100.23,
+                date: testDate  
+            },
+            {
+                originAddress: "San Francisco, CA",
+                destinationAddress: "San Diego, CA",
+                price: 100.00,
+                date: '2021/03/07'  
+            },
+        ]
          
-        return dbConn.saveJourney(params).then(() => {
-            params.price = 100.23;
-            return dbConn.saveJourney(params);
+        return dbConn.saveJourney(params[0]).then(() => {
+            return dbConn.saveJourney(params[1]).then(() => {
+                return dbConn.saveJourney(params[2]);
+            });
 
         });
 
@@ -91,6 +106,34 @@ describe('Getting daily report', () => {
             done()
         }).catch(err => {
             done(err);
+        })
+    })
+
+    describe("sql injection testing", (done) => {
+        it("should not consider OR 1=1", (done) => {
+            dbConn.getDailyReport(`${testDate} OR 1=1`).then(res => {
+                expect(res).to.not.be.null;
+                expect(res).to.be.eql({});
+                done()
+            }).catch(err => {
+                done(err);
+            })
+        })
+        it("should not allow to drop table", (done) => {
+            dbConn.getDailyReport(`${testDate}; DROP TABLE journeys`).then(res => {
+                expect(res).to.not.be.null;
+                expect(res).to.be.eql({});
+                dbConn.getDailyReport(testDate).then(res => {
+                    expect(res).to.not.be.null;
+                    expect(res.totalPrice).to.be.eql(200.23);
+                    expect(res.totalDistance).to.be.eql(1614.566)
+                    done()
+                }).catch(err => {
+                    done(err);
+                })
+            }).catch(err => {
+                done(err);
+            })
         })
     })
 });
@@ -136,10 +179,32 @@ describe("Getting date range report", () => {
             .then((res) => {
                 expect(res.totalPrice).to.be.eql(400.63);
                 expect(res.totalDistance).to.be.eql(2421.849);
-                
                 done();
             }).catch((err) => {
                 done(err);
             })
+    })
+
+    describe("SQL Injection security", () => {
+        it("should be prone for sql injection in endDate", (done) => {
+            dbConn.getDateRangeReport({startDate: '2021/03/04', endDate: '2021/03/05; OR 1=1'})
+                .then((res) => {
+                    expect(res.totalPrice).to.be.eql(200.23);
+                    expect(res.totalDistance).to.be.eql(1614.566);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                })
+        });
+
+        it("should be prone for sqlInjection in startDate", (done) => {
+            dbConn.getDateRangeReport({startDate: '2021/03/04 AND 2021/03/05; SELECT * FROM journeys WHERE date BETWEEN 2000/01/01 ', endDate: '2021/03/04'})
+                .then((res) => {
+                    expect(res).to.be.eql({})
+                    done();
+                }).catch((err) => {
+                    done(err);
+                })
+        });
     })
 })
